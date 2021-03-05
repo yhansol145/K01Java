2100a0f9933babab1bc5820a40f39f62b5818694
HANSOL <yhansol145@gmail.com>
2021-03-05 21:40:08 +0900

    add K01Java

--- src/ex02variable/E02SilsuType.java
+++ src/ex02variable/E02SilsuType.java
@@ -2,4 +2,32 @@ package ex02variable;
 
 public class E02SilsuType {
 
+	public static void main(String[] args) {
+		
+		long long1 = 100;
+		float float1 = 200;
+		float result1 = long1 + float1;
+		System.out.println("long1 + float1 = "+ result1);
+		
+		long result2 = long1 + (long)float1;
+		System.out.println("형변환후 연산="+ result2);
+		long result3 = (long)(long1 + float1);
+		System.out.println("계산후 형변환="+ result3);
+		
+		float f3 = 100;
+		
+		float f4 = (float)3.14; 
+		System.out.println("f4="+ f4);
+		
+		float f5 = 3.14F; 
+		System.out.println("f5="+ f5);
+		
+		float f6 = f3 + f4;
+		System.out.println("f6="+ f6);
+		
+		double d1 = 3.14;
+		double d2 = f6 + d1; 
+		System.out.println("d2="+ d2);
+	}
+
 }
--- src/ex02variable/E03CharBooleanType.java
+++ src/ex02variable/E03CharBooleanType.java
@@ -2,4 +2,40 @@ package ex02variable;
 
 public class E03CharBooleanType {
 
+	public static void main(String[] args) {
+		
+		char ch1 = '가';
+		String chStr = "가나다라";
+		System.out.println("ch1="+ ch1);
+		System.out.println("chStr="+ chStr);
+		
+		char ch2 = 'A'; 
+		int num1 = 2;
+		int num2 = ch2 + num1; 
+		System.out.println("num2="+ num2); 
+		System.out.println("(char)num2="+ (char)num2); 
+		
+		char ch3 = (char)(ch2+num1);
+		System.out.println("ch3(문자출력)=>"+ ch3);
+		System.out.println("ch3(아스키코드출력)=>"+ (int)ch3);
+		
+		char ch4 = '1'; 
+		char ch4_1 = '1' + 1; 
+		System.out.println("ch4"+ ch4
+				+", ch4_1="+ ch4_1
+				+", ch4_1="+ (int)ch4_1); 
+		
+		boolean bn1 = true;
+		boolean bn2 = false;
+		System.out.println("bn=1"+ bn1 +", bn2="+ bn2);
+		
+		boolean bn3 = bn1 && bn2;
+		System.out.println("bn3(And)="+ bn3); 
+		bn3 = bn1 || bn2;
+		System.out.println("bn3(Or)="+ bn3); 
+		
+		bn3 = '가' > 30000; 
+		System.out.println("bn3="+ bn3);
+	}
+
 }
