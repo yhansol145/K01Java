1ab707ea88426a635e6430a1bb77dd2d311b717d
HANSOL <yhansol145@gmail.com>
2021-03-08 23:03:20 +0900

    add BeekyoOperator

---
+++ src/ex03operator/E03BeekyoOperator.java
@@ -0,0 +1,24 @@
+package ex03operator;
+
+public class E03BeekyoOperator {
+
+	public static void main(String[] args) {
+
+		int num1=10, num2=20;
+		
+		if(num1 > num2) {
+			System.out.println("num1이 더 큽니다.");
+		}
+		else {
+			System.out.println("num2가 더 큽니다.");
+		}
+		
+		if(num1 != num2) {
+			System.out.println("num1과 num2는 다르다.");
+		}
+		else {
+			System.out.println("num1과 num2는 같다.");
+		}
+	}
+
+}
