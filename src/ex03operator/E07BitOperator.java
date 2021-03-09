d04ee2ae5dbe9ab336bc3254de7537c10d59ceb3
HANSOL <yhansol145@gmail.com>
2021-03-10 00:08:08 +0900

    add BitOperator

---
+++ src/ex03operator/E07BitOperator.java
@@ -0,0 +1,18 @@
+package ex03operator;
+
+public class E07BitOperator {
+
+	public static void main(String[] args) {
+
+		int num1 = 5; 
+		int num2 = 3; 
+		int num3 =-1; 
+					 
+		
+		System.out.println("비트AND:"+ (num1 & num2)); 
+		System.out.println("비트OR:"+ (num1 | num2)); 
+		System.out.println("비트XOR:"+ (num1 ^ num2)); 
+		System.out.println("비트Not:"+ (~num3)); 
+	}
+
+}
