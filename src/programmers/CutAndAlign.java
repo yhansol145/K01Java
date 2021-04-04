53860d2251891466c83ff7d526725d9f9eae984e
Hansol YOO <78459995+yhansol145@users.noreply.github.com>
2021-04-04 22:37:46 +0900

    incomplete

---
+++ src/programmers/CutAndAlign.java
@@ -0,0 +1,14 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class CutAndAlign {
+	
+	public int[] solution(int[] array, int[][] commands) {
+		int[] answer = {};
+		
+		
+		
+		return answer;
+	}
+}
